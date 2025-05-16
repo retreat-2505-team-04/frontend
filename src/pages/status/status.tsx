fe568be9279249f60b53b284c0f5e2634b1bb0c7
Fucskár Gergő <fucskar.gergo@alerant.hu>
2025-05-16 17:23:31 +0200

    more pages

--- src/pages/status/status.tsx
+++ src/pages/status/status.tsx
@@ -9,7 +9,7 @@ export default function StatusPage() {
     const [error, setError] = useState(null);
 
     const handleCheckStatus = () => {
-        fetch(`http://team-4.retreat.alerant.hu/api/orders/${orderId}/status`, {
+        fetch(`https://team-4.retreat.alerant.hu/api/orders/${orderId}/status`, {
             headers: {
                 Authorization: `Bearer YOUR_TOKEN_HERE`,
             },
